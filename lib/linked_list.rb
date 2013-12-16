551a940fce2f7d188f000ce9bc36c0219689dea2
Amanda Thurman <AmandaThurman@amandas-mbp.cisco>
2013-12-16 12:30:06 -0600

    refactoring indexOf method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -113,7 +113,8 @@ class LinkedList
         previous_node = node
         node = node.next_list_item
       end
-      # Get the previous node to point to the next list item after it, in order to "remove it"
+      # Get the previous node to point to the next list item after it,
+      # in order to "remove it"
       previous_node.next_list_item = node.next_list_item
       node.next_list_item = nil
     end
@@ -127,13 +128,12 @@ class LinkedList
 
     until node.next_list_item.nil? do
       return nil if node.nil?
-      if node.payload == payload
-        return i
-      else
-        node = node.next_list_item
-        i += 1
-      end
+      break if node.payload == payload
+
+      node = node.next_list_item
+      i += 1
     end
+
     return i if node.payload == payload
   end
 
